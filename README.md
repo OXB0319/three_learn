7e0d72f65fd664a4d3fe4b9afa59bce607e0b2f2
oxb <409288393@qq.com>
2018-05-18 09:58:46 +0800

    improve

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ npm run build
 ```
 
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## threejs
+``` bash
+# documnet
+https://threejs.org/docs/#manual/introduction/Creating-a-scene
+```
--- src/components/birds.vue
+++ src/components/birds.vue
@@ -1,12 +1,27 @@
 <template>
-<div>
+<div ref="test1">
 
 </div>
-
 </template>
 <script>
-  import THREE from "three"
+  import THREE from "three";
 
+  export default{
+
+      mounted:function(){
+          var scene  = new THREE.Scene();
+
+          var camera =  new THREE.PERSPECTIVE_CAMERA(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+          var renderer = new THREE.WebGLRenderer();
+
+          renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+
+      }
+
+  }
 
 </script>
 <style>
